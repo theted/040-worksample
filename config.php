d92a23ac993bde0d974e571b5c8e217ef73c93b1
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-10-06 17:36:14 +0200

    Add [sample!] DB config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Global config
+ */
+
+// define DB options as constants
+define('DB_HOST', "localhost");
+define('DB_USER', "db_user");
+define('DB_PASS', "db_pass");
+define('DB_NAME', "db_name");
+
+define('COURSES_DATA_FILE', __DIR__ . './data/data.json');
