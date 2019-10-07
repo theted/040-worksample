430bc4e63f1d86f2913a583743ba16683680f7e6
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-10-07 10:52:00 +0200

    Starting implementation of custom `course-participant` HTML element

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
-const endpoint = 'http://040.herp.se?page=data'
+const endpoint = 'https://040.herp.se?page=data'
 const addParticipantButton = document.querySelector('#add-participant')
+const participantsElem = document.querySelector('#participants')
 let numParticipants = 1
 
 // add button bindings
 addParticipantButton.addEventListener('click', event => {
-  event.preventDefault();
-  console.log('Add a new participant...')
+  event.preventDefault()
+  let newParticipant = document.createElement('course-participant')
+  newParticipant.setAttribute('id', ++numParticipants)
+  participantsElem.appendChild(newParticipant)
 })
 
   // get courses data
@@ -38,3 +41,68 @@ addParticipantButton.addEventListener('click', event => {
     })
 
   })()
+
+
+
+// define custom add participant element
+class CourseParticipant extends window.HTMLElement {
+  constructor() {
+    super()
+    const template = document.createElement('template')
+    template.innerHTML = /* html */ `
+        <div class="participant">
+          <h3>Participant</h3>
+          
+          <label for="name">Name</label>
+          <input name="name" />
+          
+          <label for="name">Phone number</label>
+          <input name="phone" />
+          
+          <label for="name">Email</label>
+          <input name="email" />
+        </div>
+      `
+
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    this._titleElem = this.shadowRoot.querySelector('h3')
+    this._nameElem = this.shadowRoot.querySelector('input[name="name"]')
+    this._phoneElem = this.shadowRoot.querySelector('input[name="phone"]')
+    this._emailElem = this.shadowRoot.querySelector('input[name="email"]')
+
+    this.init()
+  }
+
+  init() {
+    this._titleElem.textContent = 'Participant #' + this.id
+    console.log('set-element...')
+  }
+
+  setId(id) {
+    this.init()
+  }
+
+  get id() {
+    return this.getAttribute('id');
+  }
+
+  set id(newValue) {
+    this.setAttribute('id', newValue);
+  }
+
+  static get observedAttributes() {
+    return ['id'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch (name) {
+      case 'id': this.setId(newValue); break
+    }
+  }
+
+}
+
+// register custom HTML element
+window.customElements.define('course-participant', CourseParticipant)
