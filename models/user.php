6f74fee23f735d32896d66fa0f8ca3b19ab2a2e3
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-10-07 18:56:15 +0200

    Create each participant for course register in db

--- models/user.php
+++ models/user.php
@@ -16,19 +16,49 @@ class User
         extract($_POST);
         $name = $company;
 
-        try {
-            $result = $this->db->connection->prepare('INSERT INTO users (course, date, name, phone, email) VALUES (:course,:date,:name,:phone,:email)');
-            $result->execute([
-                'course' => $course,
-                'date' => $date,
-                'name' => $name,
-                'phone' => $phone,
-                'email' => $email,
-            ]);
-        } catch (\PDOException $e) {
-            echo "ERROR!";
-            print_r($e);
-            return false;
+        $result = $this->db->connection->prepare('INSERT INTO users (course, date, name, phone, email) VALUES (:course,:date,:name,:phone,:email)');
+        $result->execute([
+            'course' => $course,
+            'date' => $date,
+            'name' => $name,
+            'phone' => $phone,
+            'email' => $email,
+        ]);
+
+        $application_id = $this->db->connection->lastInsertId();
+        $haveAdditionalParticipants = true;
+        $i = 0;
+
+        // loop through participants and create a db entry for each
+        while ($haveAdditionalParticipants) {
+            ++$i;
+
+            if (isset($_POST['participant' . $i . '_name'])) {
+                echo "have participant #$i...<br>";
+
+                // create participant
+                $name = $_POST['participant' . $i . '_name'];
+                $phone = $_POST['participant' . $i . '_phone'];
+                $email = $_POST['participant' . $i . '_email'];
+
+                debug([
+                    'name' => $name,
+                    'phone' => $phone,
+                    'email' => $email,
+                ]);
+
+                // create participant in db
+                $result = $this->db->connection->prepare('INSERT INTO participants (application_id, name, phone, email) VALUES (:application_id,:name,:phone,:email)');
+                $result->execute([
+                    'application_id' => $application_id,
+                    'name' => $name,
+                    'phone' => $phone,
+                    'email' => $email,
+                ]);
+
+            } else {
+                $haveAdditionalParticipants = false;
+            }
         }
     }
 
