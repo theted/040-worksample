458ae41e81b7ccaf5147c18e3c93e55572fb3c43
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-10-09 22:57:59 +0200

    Starting switch to pug views

---
+++ data.js
@@ -0,0 +1,3 @@
+module.exports = {
+  title: '040 Worksample'
+}
--- views/layout.php
+++ views/layout.php
@@ -4,21 +4,6 @@ class Layout
 {
     public function render($data, $title = "040 worksample")
     {
-        echo '<!DOCTYPE html>
-					<html>
-						<head>
-							<meta charset="utf-8">
-							<title>' . $title . '</title>
-							<link rel="stylesheet" href="/dist/app1.0.0.css" />
-							<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
-						</head>
-						<body>
-							<a href="/?page=list">List registered users</a>
-							<a href="/">Register participant</a>
-							<div id="app">' . $data . '</div>
-							<script src="/dist/app.js"></script>
-						</body>
-					</html>
-			';
+        require_once "./views/template.php";
     }
 }
